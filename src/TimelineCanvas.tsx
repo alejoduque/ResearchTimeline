74ae230514712c16d036873fdf1d63d4bfbc0d68
alejoduque <funkzvv@gmail.com>
2025-07-23 22:36:54 -0500

    linkages again

--- src/TimelineCanvas.tsx
+++ src/TimelineCanvas.tsx
@@ -67,7 +67,7 @@ const TimelineCanvas = ({
           
           return (
             <g key={connection.id} onClick={(e) => handleConnectionClick(connection, e)} className="cursor-pointer">
-              <line x1={fromX} y1={fromY} x2={toX} y2={toY} stroke="#9CA3AF" strokeWidth="20" strokeOpacity="0" />
+              <line x1={fromX} y1={fromY} x2={toX} y2={toY} stroke="#9CA3AF" strokeWidth="40" strokeOpacity="0" />
               <line
                 x1={fromX}
                 y1={fromY}
